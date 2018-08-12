71deb2c70ad3f84d18c245e3e40aaec7be302037
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2018-08-12 03:14:27 +0200

    Block pushes Player when they are standing on top of it (hacky); Minor cleanup and tweaks

--- src/Blocks/Block.rb
+++ src/Blocks/Block.rb
@@ -14,7 +14,6 @@ module Blocks
     end
 
     def setup
-      @precision_over_performance = false
       @safe = true
     end
 
@@ -25,5 +24,29 @@ module Blocks
     def is_unsafe?
       return !is_safe?
     end
+
+    private
+
+      # NOTE:
+      # This is an _EXTREMELY_ hacky workaround for pushing the Player,
+      # when they are standing on top of this Block.
+      # Basically what we're doing is temporarily moving this Block upwards
+      # so the collision checking that happens in `super` returns the Player,
+      # if they are standing on top of this Block. Then we reset the position
+      # to the previous state to avoid any further breakage.
+      # BUT we only do this, if the method that is calling this method
+      # is the one specified below (method from my framework), to also avoid further breakage.
+      # Don't do this (except when you're in a jam, I guess that's understandable).
+      # Thank you for reading, have a nice day :)
+      def get_colliding_objects
+        return super  unless (
+          (caller.first.match(/`(\w+?)'\z/) || [])[1] == 'move_by_handle_collision_with_previous_position'
+        )
+        previous_position = get_position.dup
+        set_position y: (y - 1)
+        colliding_objects = super
+        set_position previous_position
+        return colliding_objects
+      end
   end
 end
--- src/Game.rb
+++ src/Game.rb
@@ -9,70 +9,11 @@ class Game < AdventureRL::Window
     @level.play
     @player.add_to_solids_manager @level.get_solids_manager
 
-    # TODO: tmp
-=begin
-    @blocks = [
-      Blocks::Safe.new(safe_block_settings.merge(
-        position: {
-          x: (get_side(:right)  * 0.25),
-          y: (get_side(:bottom) * 0.75),
-        },
-        size: {
-          width:  (get_size(:width)  * 0.5),
-          height: 16
-        }
-      )),
-      30.times.map do
-        next Blocks::Safe.new(safe_block_settings.merge(
-          position: {
-            x: (get_side(:left) .. get_side(:right)).sample,
-            y: (get_side(:top)  .. get_side(:bottom)).sample,
-          },
-          size: {
-            width:  (2 .. 32).sample,
-            height: (2 .. 32).sample
-          }
-        ))
-      end,
-      10.times.map do
-        next Blocks::Unsafe.new(unsafe_block_settings.merge(
-          position: {
-            x: (get_side(:left) .. get_side(:right)).sample,
-            y: (get_side(:top)  .. get_side(:bottom)).sample,
-          },
-          size: {
-            width:  (2 .. 32).sample,
-            height: (2 .. 32).sample
-          }
-        ))
-      end,
-    ].flatten
-    @blocks.each do |block|
-      add block
-    end
-=end
-
     @timer = AdventureRL::TimingHandler.new
     @timer.every seconds: 0.5 do
       puts Gosu.fps
     end
-    #@timer.every seconds: 0.1, method: method(:move_blocks)
     add @timer
-  end
-
-  def move_blocks
-    @blocks.each do |block|
-      dir = {
-        x: ((rand(2) == 0) ? 1 : -1),
-        y: ((rand(2) == 0) ? 1 : -1),
-      }
-      block.move_by(
-        x: (rand(1 .. 20) * dir[:x]),
-        y: (rand(1 .. 20) * dir[:y])
-        #x: (20 * dir[:x]),
-        #y: (20 * dir[:y])
-      )
-    end
   end
 
   def on_button_down btn
